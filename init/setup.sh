9741b86e6dda3617f1fc576cd845b890247ce5f9
soumyadeep.m <soumyadeep.m@udaan.com>
2020-08-29 05:05:15 +0530

    Improve script to use hostname from command line

--- init/setup.sh
+++ init/setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 ## Use after `init.sh` once only
+hostname=$1
 
-ssh-copy-id pi@raspberrypi.local
-
-
-
-
-
-scp conf/wpa_supplicant.conf pi@squirtle.local:/tmp/wpa_supplicant.conf
-ssh pi@squirtle.local "sudo cp /tmp/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf"
+ssh-copy-id pi@$hostname.local
+scp conf/wpa_supplicant.conf pi@$hostname.local:/tmp/wpa_supplicant.conf
+ssh pi@$hostname.local "sudo cp /tmp/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf"
+ssh pi@$hostname.local "sudo reboot now"
 
 
 
