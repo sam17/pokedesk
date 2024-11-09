e97ae3638c8db2fefbe0810fd9903fc73781915f
soumyadeep <soumyadeep9@gmail.com>
2024-11-09 21:02:20 +0530

    working with env

--- apps/ha-monitoring/run_with_env.sh
+++ apps/ha-monitoring/run_with_env.sh
@@ -1,11 +1,11 @@
-   #!/bin/bash
+#!/bin/bash
+echo "Running run_with_env.sh script"
 
-   echo "Running run_with_env.sh script"
-   # Source environment variables from the Docker environment
-   export TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN}
-   export TELEGRAM_CHAT_ID=${TELEGRAM_CHAT_ID}
-   echo "TELEGRAM_BOT_TOKEN: $TELEGRAM_BOT_TOKEN"
-   echo "TELEGRAM_CHAT_ID: $TELEGRAM_CHAT_ID"
+# Source the environment file
+. /etc/environment
 
-   # Run the Python script
-   /usr/local/bin/python /app/run.py
+echo "TELEGRAM_BOT_TOKEN: $TELEGRAM_BOT_TOKEN"
+echo "TELEGRAM_CHAT_ID: $TELEGRAM_CHAT_ID"
+
+# Run the Python script
+/usr/local/bin/python /app/run.py
